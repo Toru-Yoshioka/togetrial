67dc5f55666e1c3daff1a70f470c8f2b793573e1
TOGEKICHI-NOTE\toru <togekichi@mineo.jp>
2016-12-11 14:29:39 +0900

    TEST

--- mainte.php
+++ mainte.php
@@ -3,7 +3,40 @@ session_start();
 if (isset($_GET['mode'])) {
   unset($_SESSION['logedin']);
 } else {
-  if (! isset($_SESSION['logedin'])) {
+  if (isset($_SESSION['logedin'])) {
+    if (isset($_POST['packet_size']) and isset($_POST['gift_code']) and isset($_POST['created_date'])) {
+      // パケギフ登録
+      $conn = "host=ec2-23-23-199-72.compute-1.amazonaws.com dbname=d25481250mtets user=mtrdhlivfehdrj password=lhXZgchb6JgtNPmToWmF3yaZlh";
+      $link = pg_connect($conn);
+      if (!$link) {
+        die('接続失敗です。'.pg_last_error());
+      }
+      // 接続に成功
+      $result = pg_query('
+INSERT INTO
+ togepgift
+ (
+  code,
+  created_date,
+  packet_size
+ ) VALUES (
+  \'' . $gift_code . '\',
+  \'' . $created_date . '\',
+  ' . $packet_size . '
+ )
+      ');
+      if (!$result) {
+        die('クエリーが失敗しました。'.pg_last_error());
+      } else {
+        $insert_result = 'パケットギフトを登録しました。[' . $gift_code . ']';
+      }
+      $close_flag = pg_close($link);
+
+      if ($close_flag){
+//     print('切断に成功しました。<br>');
+      }
+    }
+  } else {
     if(isset($_POST['loginkey'])){
       if ($_POST['loginkey'] == 'togesmasmainte') {
         $_SESSION['logedin'] = 'mainte';
@@ -79,14 +112,17 @@ if (isset($_GET['mode'])) {
         <option value="100">100</option>
         </select><br/>
         <br/>
-        <h2>ギフトコード</h2> <input type="text" style="height: 32px; width: 240px; font-size: x-large;" name="gift_code"/><br/>
+        <h2>ギフトコード</h2> <input type="text" style="height: 64px; width: 240px; font-size: x-large;" name="gift_code"/><br/>
         <br/>
-        <h2>ギフト作成日</h2> <input type="text" style="height: 32px; width: 240px; font-size: x-large;" name="created_date" value="<?php print($now_date); ?>"/><br/>
+        <h2>ギフト作成日</h2> <input type="text" style="height: 64px; width: 240px; font-size: x-large;" name="created_date" value="<?php print($now_date); ?>"/><br/>
         <br/>
-        <input type="submit" style="width: 320px; height: 32px;" value="登録"/>
+        <input type="submit" style="width: 320px; height: 80px;" value="登録"/>
         <br/>
         <br/>
-        <input type="button" style="width: 320px; height: 32px; font-size: x-large;" onClick="location.href='./mainte.php?mode=logout'" value="ログアウト"/>
+        <h2><?php print($insert_result); ?></h2>
+        <br/>
+        <br/>
+        <input type="button" style="width: 320px; height: 80px; font-size: x-large;" onClick="location.href='./mainte.php?mode=logout'" value="ログアウト"/>
       </form>
     </div>
 <?php
@@ -96,9 +132,9 @@ if (isset($_GET['mode'])) {
     <div class="gift_box_area">
       <h1>ログイン</h1>
       <form method="post" action="./mainte.php">
-        <input type="password" style="height: 32px; width: 320px; font-size: x-large;" name="loginkey"/><br/>
+        <input type="password" style="height: 64px; width: 320px; font-size: x-large;" name="loginkey"/><br/>
         <br/>
-        <input type="submit" style="width: 320px; height: 32px; font-size: x-large;" value="ログイン"/>
+        <input type="submit" style="width: 320px; height: 80px; font-size: x-large;" value="ログイン"/>
       </form>
     </div>
 <?php
