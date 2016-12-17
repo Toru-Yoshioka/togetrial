05a89a9d3fceec961674894796690ae5cb0cab92
TOGEKICHI-NOTE\toru <togekichi@mineo.jp>
2016-12-18 00:56:33 +0900

    index表示確認用モジュールをアップ

--- indexcheck.php
+++ indexcheck.php
@@ -73,8 +73,8 @@ if ($close_flag){
       display: block;
       float:left;
       border: solid 1px #ffffff;
-      width: 15%;
-      height: 12%;
+      width: 12%;
+      height: 10%;
     }
     .item_gallery_title {
       clear: both;
